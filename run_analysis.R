7409992516a91cf72ea27481ee56a80086939169
Marco Mesturino <mmest@tiscalinet.it>
2014-11-23 05:29:02 +0100

    reduced data set to mean std only

--- run_analysis.R
+++ run_analysis.R
@@ -42,61 +42,96 @@ cleanUpData <- function(rawDataDirName = "UCI HAR Dataset") {
 }
 
 
-## Function 'runAnalysis()' combines train and test sets into one and generates the tidy data sets. 
+## Function 'runAnalysis()' combines train and test data sets into one and generates the tidy data set
+##           which contains the average of each variable for each activity and each subject.
 ##
-## Returns the name of the final tidy data set with the average of each variable for each activity and each subject.
+## Returns the name of the final tidy data set 
 ## 
 runAnalysis <- function() {
   
-  # Make sure we have the raw data: 
+  ftDT <- NULL   # Cached data frame of feature names, loaded initially from 'mergeRawData()'. 
+  
+  ## Function 'mergeRawData()' combines columns from the test and train files. It also
+  ##           assigns meaningful names to the columns, and it stores the feature names
+  ##           into variable 'ftDT' which is cached into the parent frame for reuse on 
+  ##           subsequent calls. 
+  ## Parameters: 'dt' combined data frame;
+  ##             'dirName' path to the raw data directory;
+  ##             'setName' the suffix used for files and subdirectories ("train" or "test"). 
+  ## Returns: combined data frame. 
+  ## Side effects: 'ftDT'.
+  ## 
+  mergeRawData <- function(dt, dirName, setName)  {
+    # Combine and name columns from subject_*.txt 
+    dt <- read.table(file.path(dirName, setName, paste0("subject_", setName, ".txt")), 
+                     col.names = "subjectID", comment.char = "")
+    # Combine and name columns from y_*.txt
+    dt <- cbind(dt, 
+                read.table(file.path(dirName, setName, paste0("y_", setName, ".txt")), 
+                           col.names = "activity", comment.char = "") )
+    # Get feature names to use as x_*.txt column names: 
+    if (is.null(ftDT)) {  # Load and cache ftDT in parent frame, if not already. 
+      ftDT <<- read.table(file.path(dirName, "features.txt"), 
+                          stringsAsFactors = FALSE, col.names = c("ID", "feature"), sep = " ", comment.char = "")
+      # remove some characters from names, replace '-' with '.' and make sure are unique: 
+      ftDT$feature <<- make.names(gsub("[,()]+", "", ftDT$feature), unique = TRUE) 
+    }
+    # Combine and name columns from x_*.txt
+    dt <- cbind(dt, 
+                read.table(file.path(dirName, setName, paste0("x_", setName, ".txt")), 
+                           col.names = ftDT$feature, comment.char = "") )
+    
+    return(dt)
+  } # mergeRawData()
+  
+  # Start of runAnalysis() 
+  
+  # 0. Make sure we have the raw data: 
   message("Start of analysis run...")
   rawDataDirName <- getData()   
   message("... raw data is in: ", rawDataDirName)
   
-  # 1. Merge the training and data set into one. Assign meaningful column names and classes. 
+  # 1. Merge the training and data set into one. 
+  # 4 .Assign meaningful column names and classes. 
   message("... merge of datasets ...")
   dt <- data.frame()
-  # Merge of all columns from the test files: 
+  message( "... merge of all columns from the test files ..." )
   dt <- mergeRawData(dt, rawDataDirName, "test")
-  # Merge all cols from the train files, add rows to combined data frame: 
-  dt <- rbind(dt, mergeRawData(dt, rawDataDirName, "train"))
+  message("... merge all cols from the train files, add rows to combined data frame ...")
+  message(" ... this will take time ...")
+###TEST  dt <- rbind(dt, mergeRawData(dt, rawDataDirName, "train"))
   
   # 2. Extracts the measurements on the mean and standard deviation for each measurement:
-  
+  message("... reducing data frame to mean() and std() measurements ...")
+  dt <- dt[ , c( "subjectID", "activity", ftDT[grep("\\.mean|\\.std", ftDT$feature) , "feature"] ) ]
   
   # 3. Load activity ID numbers and descriptive labels from file:  
+  message("... adding meaningful activity names ...")
   actDT <- read.table(file.path(rawDataDirName, "activity_labels.txt"), 
                       col.names = c("ID", "label"), comment.char = "")
   # Convert numeric IDs of the 'activity' column of combined data to a factor whose
   # levels match the descriptive labels from the file: 
   dt$activity <- factor(dt$activity, actDT$ID, actDT$label, ordered = FALSE)
   
-  # Make the tidy data file: 
+  # 5. Make the tidy data file: 
   message("... generating tidy data set ...")
-  
+  dt <- makeTidy(dt)
   message("... witing tidy data file ...")
   tidyFileName <- writeTidyFile(dt)
   
   message("... END RUN. Tidy data is in: ", tidyFileName)
-  return(tidyFileName)
   
 } # runAnalysis()
 
-
-mergeRawData <- function(dt, dirName, setName)  {
-  
-  dt <- read.table(file.path(dirName, setName, paste0("subject_", setName, ".txt")), 
-                   col.names = "subjectID", comment.char = "")
-  dt <- cbind(dt, 
-              read.table(file.path(dirName, setName, paste0("y_", setName, ".txt")), 
-                         col.names = "activity", comment.char = "") )
-
+makeTidy <- function(dt) {
   
   return(dt)
 }
 
 
 writeTidyFile <- function(dt, tidyFileName = "tidy_data.txt") {
+  # Write 'dt' as a space-delimited file
+  write.table(dt, tidyFileName, sep = "\t", row.name=FALSE)
   
   return(tidyFileName)
 }
